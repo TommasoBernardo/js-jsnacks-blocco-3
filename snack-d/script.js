2a94cf18e7413d67f50373637130ff216076998b
TommasoBernardo <tommasob1210@gmail.com>
2022-11-28 17:27:43 +0100

    update

--- snack-d/script.js
+++ snack-d/script.js
@@ -7,9 +7,9 @@ const arrayElement2 = [5, 10];
 
 let sum = 0;
 
-while(arrayElement1 != arrayElement2 ){
+while(arrayElement2.lenght != arrayElement1.length ){
     let number = parseInt(prompt("inserisci un numero"));
-    arrayElement2.push(number);
+    number.push(arrayElement2);
 }
 
 console.log(arrayElement1);
