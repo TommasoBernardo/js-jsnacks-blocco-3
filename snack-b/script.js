5290b277555b2b49a104d625fde09e33c88b4044
TommasoBernardo <tommasob1210@gmail.com>
2022-11-28 17:03:39 +0100

    update number

--- snack-b/script.js
+++ snack-b/script.js
@@ -2,9 +2,9 @@
 
 
 
-let number;
+let number = 0;
 while((number > 10) || (number < 1) ){
-    number = parseInt(prompt("inserisci il numero corretto"))
+    number = parseInt(prompt("inserisci il numero corretto"));
 }
 
 console.log(number);
