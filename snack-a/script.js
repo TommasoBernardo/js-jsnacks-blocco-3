bb916a96b1f8b6e3030cda10c813abb7320de772
TommasoBernardo <tommasob1210@gmail.com>
2022-11-28 15:40:00 +0100

    finish shooping list

---
+++ snack-a/script.js
@@ -0,0 +1,11 @@
+//Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista 
+//individualmente con un ciclo while.
+
+const list = ['Frutta', 'verdura' , 'Carne' , 'Pesce'];
+
+let  i=0;
+
+while (i<list.length) {
+    console.log(list[i]); 
+    i++;
+}
