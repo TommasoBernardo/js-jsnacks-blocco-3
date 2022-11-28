a2c15f5006c942091d7019de31abab8a806a7324
TommasoBernardo <tommasob1210@gmail.com>
2022-11-28 17:37:05 +0100

    update

--- snack-d/script.js
+++ snack-d/script.js
@@ -1,15 +1,17 @@
 //Crea due array che hanno un numero di elementi diversi. Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà 
 //tanti quanti l'altro.
 
-const arrayElement1 = [5, 10, 12, 13];
+let arrayElement1 = [5, 10, 12, 13];
 
-const arrayElement2 = [5, 10];
+let arrayElement2 = [5, 10];
 
 let sum = 0;
 
-while(arrayElement2.lenght != arrayElement1.length ){
+while(arrayElement2.lenght != arrayElement1.length ){   
     let number = parseInt(prompt("inserisci un numero"));
-    number.push(arrayElement2);
+    arrayElement2.push(number);
+
+    arrayElement1.push(number); 
 }
 
 console.log(arrayElement1);
